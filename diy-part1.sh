b7c292b724883ba49f6e6de1421a262f63201e23
Mahiru <41849505+Starinks@users.noreply.github.com>
2021-05-20 13:50:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,3 +27,5 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb #
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+git clone https://github.com/fw876/helloworld.git package/helloworld
